530c8bfebd1b465b081e39a1ca8fabea3c460f84
Senny <senny8617@gmail.com>
2022-10-24 18:09:12 +0800

    Add View for backstage (audit page not complete)

---
+++ 3TeamProject/Controllers/BackstageController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _3TeamProject.Controllers
+{
+    public class BackstageController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Admin()
+        {
+            return View();
+        }
+        public IActionResult Member()
+        {
+            return View();
+        }
+        public IActionResult Supplier()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return View();
+        }
+        public IActionResult Attraction()
+        {
+            return View();
+        }
+        public IActionResult Community()
+        {
+            return View();
+        }
+    }
+}
